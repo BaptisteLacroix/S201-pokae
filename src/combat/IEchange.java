4b18f4cd1ce0593f56f9d328d02798128175e738
Baptiste LACROIX <61122159+BaptisteLacroix@users.noreply.github.com>
2022-05-12 13:34:39 +0200

    adding interface

---
+++ src/combat/IEchange.java
@@ -0,0 +1,20 @@
+/**
+ * Université Côte d'Azur
+ * IUT Côte d'Azur
+ * Département Informatique
+ * @date
+ * IEchange.java
+ */
+package interfaces.combat;
+
+import interfaces.combat.IAttaque;
+import interfaces.pokemon.IPokemon;
+
+/**
+ * @author Leo Donati
+ *
+ */
+public interface IEchange extends IAttaque {
+	public void setPokemon(IPokemon pok);
+	public IPokemon echangeCombattant(); 
+}
