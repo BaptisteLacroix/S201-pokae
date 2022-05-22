f5a3284ab7942430f67266393856aad4a6d32b39
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2022-05-22 18:01:32 +0200

    Dev lacroix (#17)
    * Début d'instanciation des classes
    
    * mise en place de l'implémentation des interfaces
    
    * modification methode initializeCSV
    
    * adding a new method + creat enum Type
    
    * modification de initializeFromCSV ne prend maintenant que les especes de niveau 1
    
    * methode engendreRanch finie
    
    * methode getEfficacite en cours de developpement, problème de csv a trouver
    
    * methode getEfficacite finis
    
    * methode getEfficacite finis
    
    * ajout de la méthode getCapSet il faut vérifier qu'elle fonctionne bien
    
    * solving issue
    
    * methode getCapSet faite
    
    * update
    
    * methode getEvolution faite
    
    * methode getInfo faite
    
    * les 2 méthodes getCapacite faites
    
    * ajout des dernières modifications mineurs sur le projet
    
    * Classe Pokemon,Espece, pokedex finies
    
    * ajout de la JavaDoc
    
    * JUnit faite
    
    * methode main, javaDoc et Junit modifiées
    
    * ajout des différents cas particuliers d'attaques
    
    * Classe Main faite
    
    * Finalisation des test Junit, du main. Réduction du temps chargement suite a la création d'une classe créant un fichier csv contenant toutes les capacités pour une espèce
    
    * ajout javaDoc

---
+++ src/main/java/statsPokemon/Categorie.java
@@ -0,0 +1,51 @@
+/**
+ * Université Côte d'Azur
+ * IUT Côte d'Azur
+ * Département Informatique
+ * @date
+ * ICategory.java
+ */
+package statsPokemon;
+
+import interfaces.ICategorie;
+
+import java.util.Objects;
+
+/**
+ * @author Lacroix Baptiste
+ * Il s'agit de la catégorie d'une capacité :
+ *  - soit Physique
+ *  - soit Special
+ */
+public enum Categorie implements ICategorie {
+	Physique("Physique"),
+	Special("Special");
+	private String nom;
+
+
+	Categorie(String nom) {
+		this.nom = nom;
+	}
+
+
+	/**
+	 * Si le nom de l'objet en cours est égal au nom de l'objet passé en paramètre, alors renvoie vrai, sinon renvoie faux
+	 *
+	 * @return Le nom de l'objet.
+	 */
+	@Override
+	public boolean isSpecial() {
+		return Objects.equals(this.nom, Physique.nom);
+	}
+
+
+	/**
+	 * Cette fonction renvoie le nom de la Catégorie
+	 *
+	 * @return Le nom de la Catégorie.
+	 */
+	@Override
+	public String getNom() {
+		return this.nom;
+	}
+}
