369907a44d3741b4a495ef99ee7b15b8bbf8c279
BaptisteLacroix <baptiste.lacroix03@gmail.com>
2022-06-01 09:58:31 +0200

    petit chaangement dans chrono rajout des ms

--- src/main/java/writingCSV/Chrono.java
+++ src/main/java/writingCSV/Chrono.java
@@ -2,62 +2,63 @@ package writingCSV;
 
 public class Chrono {
 
-    private long tempsDepart=0;
-    private long tempsFin=0;
-    private long pauseDepart=0;
-    private long pauseFin=0;
-    private long duree=0;
+    private long tempsDepart = 0;
+    private long tempsFin = 0;
+    private long pauseDepart = 0;
+    private long pauseFin = 0;
+    private long duree = 0;
 
-    public void start()
-    {
-        tempsDepart=System.currentTimeMillis();
-        tempsFin=0;
-        pauseDepart=0;
-        pauseFin=0;
-        duree=0;
+    public void start() {
+        tempsDepart = System.currentTimeMillis();
+        tempsFin = 0;
+        pauseDepart = 0;
+        pauseFin = 0;
+        duree = 0;
     }
 
-    public void pause()
-    {
-        if(tempsDepart==0) {return;}
-        pauseDepart=System.currentTimeMillis();
+    public void pause() {
+        if (tempsDepart == 0) {
+            return;
+        }
+        pauseDepart = System.currentTimeMillis();
     }
 
-    public void resume()
-    {
-        if(tempsDepart==0) {return;}
-        if(pauseDepart==0) {return;}
-        pauseFin=System.currentTimeMillis();
-        tempsDepart=tempsDepart+pauseFin-pauseDepart;
-        tempsFin=0;
-        pauseDepart=0;
-        pauseFin=0;
-        duree=0;
+    public void resume() {
+        if (tempsDepart == 0) {
+            return;
+        }
+        if (pauseDepart == 0) {
+            return;
+        }
+        pauseFin = System.currentTimeMillis();
+        tempsDepart = tempsDepart + pauseFin - pauseDepart;
+        tempsFin = 0;
+        pauseDepart = 0;
+        pauseFin = 0;
+        duree = 0;
     }
 
-    public void stop()
-    {
-        if(tempsDepart==0) {return;}
-        tempsFin=System.currentTimeMillis();
-        duree=(tempsFin-tempsDepart) - (pauseFin-pauseDepart);
-        tempsDepart=0;
-        tempsFin=0;
-        pauseDepart=0;
-        pauseFin=0;
+    public void stop() {
+        if (tempsDepart == 0) {
+            return;
+        }
+        tempsFin = System.currentTimeMillis();
+        duree = (tempsFin - tempsDepart) - (pauseFin - pauseDepart);
+        tempsDepart = 0;
+        tempsFin = 0;
+        pauseDepart = 0;
+        pauseFin = 0;
     }
 
-    public long getDureeSec()
-    {
-        return duree/1000;
+    public long getDureeSec() {
+        return duree / 1000;
     }
 
-    public long getDureeMs()
-    {
+    public long getDureeMs() {
         return duree;
     }
 
-    public String getDureeTxt()
-    {
+    public String getDureeTxt() {
         return timeToHMS(getDureeSec());
     }
 
@@ -69,13 +70,22 @@ public class Chrono {
         int h = (int) (tempsS / 3600);
         int m = (int) ((tempsS % 3600) / 60);
         int s = (int) (tempsS % 60);
+        int ms = (int) (tempsS);
 
-        String r="";
+        String r = "";
 
-        if(h>0) {r+=h+" h ";}
-        if(m>0) {r+=m+" min ";}
-        if(s>0) {r+=s+" s";}
-        if(h<=0 && m<=0 && s<=0) {r="0 s";}
+        if (h > 0) {
+            r += h + " h ";
+        }
+        if (m > 0) {
+            r += m + " min ";
+        }
+        if (s > 0) {
+            r += s + " s";
+        }
+        if (h <= 0 && m <= 0 && s <= 0) {
+            r += ms + " s";
+        }
 
         return r;
     }
